88bf80e3957755420b291a5ffe07d7fda89f9ceb
Martin Wheeler <martin@martinwheeler.com.au>
2020-05-31 11:30:03 +1000

    feat: allow new data points dynamically

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 import { Line } from "react-chartjs-2";
+import randomColor from 'randomcolor'
 
 const getData = () => {
   return fetch("http://localhost:3000/data").then((response) =>
@@ -9,69 +10,86 @@ const getData = () => {
   );
 };
 
+const getBorderColour = (key) => {
+  return randomColor({ seed: key, format: 'rgb', luminosity: 'bright' });
+}
+
 const POLL_TIMEOUT = 1000;
+const defaultDatasetOptions = {
+  label: 'Undefined',
+  fill: false,
+  lineTension: 0.1,
+  borderColor: "rgb(199, 22, 22)",
+  borderWidth: 1,
+  data: []
+};
 
 class App extends Component {
-  state = {
-    labels: [],
-    datasets: [
-      {
-        label: "Pitch",
-        fill: false,
-        lineTension: 1,
-        borderColor: "rgb(0,0,0)",
-        borderWidth: 2,
-        data: [],
-      },
-      {
-        label: "Roll",
-        fill: false,
-        lineTension: 25,
-        borderColor: "rgb(235, 161, 2)",
-        borderWidth: 2,
-        data: [],
-      },
-      {
-        label: "Yaw",
-        fill: false,
-        lineTension: 0.1,
-        borderColor: "rgb(67, 181, 25)",
-        borderWidth: 2,
-        data: [],
-      },
-      {
-        label: "PitchRate",
-        fill: false,
-        lineTension: 0.1,
-        borderColor: "rgb(50, 117, 168)",
-        borderWidth: 2,
-        data: [],
-      },
-      {
-        label: "RollRate",
-        fill: false,
-        lineTension: 0.1,
-        borderColor: "rgb(139, 50, 168)",
-        borderWidth: 2,
-        data: [],
-      },
-      {
-        label: "YawRate",
-        fill: false,
-        lineTension: 0.1,
-        borderColor: "rgb(199, 22, 22)",
-        borderWidth: 2,
-        data: [],
-      },
-    ],
-    shouldPoll: true,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      labels: [],
+      datasets: [],
+      shouldPoll: true,
+      shouldPersist: true
+    };
+
+    this.mapDataToDataset = this.mapDataToDataset.bind(this);
+  }
+
+  mapDataToDataset(newData) {
+    const { labels, datasets } = this.state;
+
+    const newLabels = labels.concat(newData.map(data => data.timestamp));
+    const newDatasets = datasets.reduce((result, currentDataset) => {
+      return {
+        ...result,
+        [currentDataset.label]: {
+          ...currentDataset
+        }
+      }
+    }, {})
+
+    newData.forEach((currentDataEntry) => {
+      const allKeysExcludingTimestamp = Object.keys(currentDataEntry).filter(key => key !== 'timestamp');
+
+      allKeysExcludingTimestamp.forEach(currentKey => {
+        if (newDatasets[currentKey]) {
+          newDatasets[currentKey] = {
+            ...newDatasets[currentKey],
+            data: [
+              ...newDatasets[currentKey].data,
+              currentDataEntry[currentKey]
+            ]
+          }
+        } else {
+          newDatasets[currentKey] = {
+            ...defaultDatasetOptions,
+            label: currentKey,
+            borderColor: getBorderColour(currentKey),
+            data: [
+              currentDataEntry[currentKey]
+            ]
+          }
+        }
+      });
+    });
+
+    this.setState({ labels: newLabels, datasets: Object.values(newDatasets) });
+  }
 
   componentDidMount() {
     document.addEventListener("keypress", (e) => {
       // Space
       if (e.which === 32) {
         this.setState({ shouldPoll: !this.state.shouldPoll });
+      }
+      if (e.which === 'P') {
+        this.setState({ shouldPersist: !this.state.shouldPersist });
+      }
+      if (e.which === 'C') {
+        // TODO: Clear localStorage
       }
     });
 
@@ -100,7 +118,7 @@ class App extends Component {
         position: "right",
       },
       scales: {
-        xAxes: [{ type: "time", time: { unit: "minutes", stepSize: 1000 }, distribution: "series", ticks: { minRotation: 1000, maxRotation: 1000 } }]
+        xAxes: [{ type: "time" }]
       },
       tooltips: {
         mode: 'index'
